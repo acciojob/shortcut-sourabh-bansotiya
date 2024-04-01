39324f5a8c614850f62fdbbf4dc90626113275f8
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-02 04:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 function shortcut(s1, s2) {
   // your code here
-	for (let i = 0; i < s1.length; i++) {
-		for (let j = 0; j < s2.length; j++) {
-			if (s1[i] != ' ') {
-				return s1[i] + s2[j];
-			}
-			else{
-				return ' ';
-			}
-			else if (s2[j] != ' ') {
-				return s1[i] + s2[j];
-			}
-			else{
-				return ' ';
-			}
-		}
-	}
+	//for (let i = 0; i < s1.length; i++) {
+	//	for (let j = 0; j < s2.length; j++) {
+	//		if (s1[i] != ' ') {
+	//			return s1[i] + s2[j];
+	//		}
+	//		else{
+	//			return ' ';
+	//		}
+	//		else if (s2[j] != ' ') {
+	//			return s1[i] + s2[j];
+	//		}
+	//		else{
+	//			return ' ';
+	//		}
+	//	}
+	//}
+
+	if (s1 && s2) {
+        return s1[0] + s2[0];
+    } else {
+        return '';
+    }
 }
 
 // Do not change the code below.
